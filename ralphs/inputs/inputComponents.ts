afc4579bf53e1decd4664376c5227fbaf7cb12b2
Mirsad Avdic <40442379+rosinant3@users.noreply.github.com>
2022-09-20 08:26:18 +0200

    Update inputComponents.ts

--- ralphs/inputs/inputComponents.ts
+++ ralphs/inputs/inputComponents.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const ColorContainer = styled.div``;
 
+export const ContentContainer = styled.div``;
+
 export const InfoContainer = styled.div`
 	display: flex;
 	flex-direction: column;
