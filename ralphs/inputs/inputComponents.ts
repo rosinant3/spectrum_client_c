9bd2f015659e7c1f3e86aca13e4223842d167312
Mirsad Avdic <40442379+rosinant3@users.noreply.github.com>
2022-09-20 08:24:09 +0200

    Create inputComponents.ts

---
+++ ralphs/inputs/inputComponents.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ColorContainer = styled.div``;
+
+export const InfoContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`;
+
+export const DateContainer = styled.div``;
+
+export const TypeContainer = styled.div``;
