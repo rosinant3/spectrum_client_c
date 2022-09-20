913082cc88c0ffa9cfe2b4b378e45c2617e2072f
Mirsad Avdic <40442379+rosinant3@users.noreply.github.com>
2022-09-20 08:29:42 +0200

    Create datepicker.tsx

---
+++ ralphs/inputs/datepicker/datepicker.tsx
@@ -0,0 +1,33 @@
+import React, { useState, SetStateAction  } from 'react';
+import { InfoContainer, DateContainer } from '../inputComponents';
+import DateFnsUtils from '@date-io/date-fns';
+import { DateTimePicker, MuiPickersUtilsProvider, } from '@material-ui/pickers';
+
+type selectedDate = Date | null;
+
+interface DatePickerProps { 
+  dateTime: { value: Date, error: string; };
+	setSelectedDate: SetStateAction <selectedDate>;
+	selectedDate: selectedDate;
+};
+
+const DatePicker: React.FC<DatePickerProps> = (props, ref:any) => {
+  return (<InfoContainer>
+		<DateContainer>
+			<MuiPickersUtilsProvider utils={DateFnsUtils}>
+			  <DateTimePicker
+			  
+          variant="inline"
+          value={props.selectedDate}
+          onChange={props.setSelectedDate}
+          format="MM/dd/yyyy HH:mm"
+          margin="normal"
+          label={"Date:"}
+				
+			  />
+			</MuiPickersUtilsProvider>
+		</DateContainer>
+  </InfoContainer>);
+};
+
+export default DatePicker;
