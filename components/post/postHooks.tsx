af1f0a1581a4820a9dff763ff4b7fcb8ebb1fb1a
Mirsad Avdic <40442379+rosinant3@users.noreply.github.com>
2022-09-21 09:10:43 +0200

    Update postHooks.tsx

--- components/post/postHooks.tsx
+++ components/post/postHooks.tsx
@@ -1,31 +1,46 @@
 import { useState, useEffect, useRef, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { incidentFormTypes } from '../redux/interface';
 
-export const useSelectedDate = (dateTime: any) => {
-    const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
-    useEffect(()=>{
-      setSelectedDate(dateTime.value);
-    }, [ dateTime ])  
-    return {
-      selectedDate,
-      setSelectedDate
-    }
+export const useIncidents = ({ id, incident }: { id:number, incident:number }) => {
+	return useSelector((state: any)=>{
+		const post = state.incidentForm;
+		return post;
+	  });
 };
-  
+/* refactoring needed? */
 export const usePostSubmit = ({ id, selectedDate }: { id: number, selectedDate: Date | null }) => {
+    
+    const { postGraphics, hasGraphics } = useSelector((state:any)=>{
+      const incidents: incidentFormTypes = state.incidentForm;
+      const postGraphics = incidents.postGraphics.items;
+      const hasGraphics = postGraphics.length > 0;
+      return { postGraphics, hasGraphics };
+    });
+    
     const [ buttonText, setButtonText ] = useState('');
     const contentRef = useRef(null);
     const colorRef = useRef(null);
+
     useEffect(()=>{
       if (id == -1) {
         setButtonText('Create');
-      } else {
-        setButtonText('Update');
+      } else if (id > -1) {
+        setButtonText('Publish');
       }
     }, [id]);
+
     const submitForm = useCallback((e: any)=>{
       e.preventDefault();
-      /* submit data from refs */
-    }, [contentRef, colorRef, selectedDate])
+      
+      console.log(selectedDate);
+      console.log(contentRef);
+      console.log(colorRef);
+      console.log(postGraphics);
+  
+  
+    }, [contentRef, colorRef, selectedDate, postGraphics, hasGraphics]);
+
     return { 
       buttonText, 
       submitForm,
