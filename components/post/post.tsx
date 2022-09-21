26a7f2975bac064d9d7b89ff166c839e951b1772
Mirsad Avdic <40442379+rosinant3@users.noreply.github.com>
2022-09-21 09:17:24 +0200

    Update post.tsx

--- components/post/post.tsx
+++ components/post/post.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import {    
-	
-  PostIncidentCont,
-  PostHeader,
+ 
+  IncidentFormCont,
+  IncidentFormHeader,
   ButtonContainerRight,
   PostButton
-	
 } from '../mapComponents';
 import { useParams } from 'react-router-dom';
 import Color from '../../../../ralphs/inputs/color/color';
 import DatePicker from '../../../../ralphs/inputs//datePicker/datepicker';
-import Content from '../../../../ralphs/inputs/content/content';
-import { useIncident } from '../utils/incidentUtility';
+import Content from '../../../../ralphs/inputs/content/content'
 //import MediaEditor from '../../../../ralphs/media/mediaEditorComponent';
-import { useSelectedDate, usePostSubmit } from './postIncidentHooks';
+import { usePostSubmit, useIncidents } from './postIncidentHooks';
+import { useSelectedDate } from '../../../../ralphs/hooks/selectedDateHook';
 
-interface PostIncidentProps {
+interface IncidentFormProps {
   setShowPostOnClick: any;
   incident: number;
 };
 
-const PostIncident: React.FC<PostIncidentProps> = (props) => {
+const IncidentForm: React.FC<IncidentFormProps> = (props) => {
 
   const params = useParams();
   const id: any = params.id;
@@ -28,18 +27,25 @@ const PostIncident: React.FC<PostIncidentProps> = (props) => {
   const post = useIncident({ id, incident });
   const dateTime = post.dateTime;
   const { selectedDate, setSelectedDate } = useSelectedDate(dateTime);
-  const { buttonText, submitForm, contentRef, colorRef } = usePostSubmit({ id: post.id, selectedDate: selectedDate });
+  const { 
 
-  return (<PostIncidentCont onSubmit={submitForm}>
+      buttonText, 
+      submitForm,
+      contentRef,
+      colorRef
+
+  } = usePostSubmit({ id: post.id, selectedDate: selectedDate });
+
+  return (<IncidentFormCont onSubmit={submitForm}>
 		<ButtonContainerRight>
 			<PostButton type="submit" >{buttonText}</PostButton> 
 		</ButtonContainerRight>
-		<PostHeader>
+      		<IncidentFormHeader>
 			<DatePicker dateTime={dateTime} selectedDate={selectedDate} setSelectedDate={setSelectedDate}  />
-			<Color color={post.color} ref={colorRef}/>
-		</PostHeader>
+        		<Color color={post.color} ref={colorRef}/>
+      		</IncidentFormHeader>
       		<Content content={post.content} ref={contentRef} />
-    </PostIncidentCont>);
+    </IncidentFormCont>);
 };
 
 /*  <MediaEditor incident={incident} id={id} />  */
